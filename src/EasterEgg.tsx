87ff8b0d3a702f1368e24f040f0206e109e34bf9
Emma <ejamieson19@gmail.com>
2020-08-26 22:45:26 +0100

    update type

--- src/EasterEgg.tsx
+++ src/EasterEgg.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 type SnowFlake = {
-  id: string,
+  id: number,
   style: {},
 }
 
@@ -19,13 +19,13 @@ function Snow() {
   let animationDelay = '0s';
   let fontSize = '100px';
   let arr = Array.from('Haha well done! You have found my easter egg - enjoy the snow. Thanks for stopping by, see you round :)')
-      return arr.map((el, i)=>{
-    animationDelay = `${(Math.random()*16).toFixed(2)}s`;
-    fontSize = `${(Math.floor(Math.random()*10) + 10)}px`;
-    let style = { animationDelay, fontSize };
-  return (<Snowflake key={i} id={i} style={style}/>)
-  });
-}
+    return arr.map((el, i) => {
+      animationDelay = `${(Math.random()*16).toFixed(2)}s`;
+      fontSize = `${(Math.floor(Math.random()*10) + 10)}px`;
+      let style = { animationDelay, fontSize };
+      return (<Snowflake key={i} id={i} style={style}/>)
+    });
+};
 
 function EasterEgg() {
   return (
