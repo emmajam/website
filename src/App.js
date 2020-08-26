e0ec2b13ba96ee208163549b8f50b593ab93fab7
Emma <ejamieson19@gmail.com>
2020-08-26 18:23:49 +0100

    Personalise, tidyup

--- src/App.js
+++ src/App.js
@@ -14,42 +14,13 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
+      <h1>Emma Jamieson-Hoare</h1>
+      <h2>Front End Engineer</h2>
+      <ul>
+        <li><a href="https://www.skyscanner.net">Skyscanner</a> (July 2018 - present)</li>
+        <li><a href="https://www.localz.com/">Localz</a> (August 2016 - July 2018)</li>
+      </ul>
       <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
